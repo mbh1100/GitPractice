2ae0b694cf77d8eb1db9e5760758a5f18123f862
Thor Smith <smith777@purdue.edu>
2020-03-07 13:26:25 -0500

    adds a help function to the App

--- PracticeProject/src/app/App.java
+++ PracticeProject/src/app/App.java
@@ -2,6 +2,13 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Woah there Java");
+    }
+
+    public void help() {
+      System.out.println("This is a help message.\n");
+      for (int i = 0; i < 22; i++) {
+        System.out.println("Help " + i + " message.\n");
+      }
     }
 }
